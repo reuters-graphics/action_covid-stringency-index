836408fa21fc00d0274d8bf556655fb87bed0d40
Basile Simon <basile.simon@thomsonreuters.com>
2020-04-03 14:26:10 +0200

    Rebuild simpler array of objects from parsed data

--- index.js
+++ index.js
@@ -7,32 +7,44 @@ const xlsx = require('node-xlsx');
 
 const SOURCE_PATH = path.resolve(__dirname, 'data/latest.xlsx');
 async function downloadLatestXLSX() {
-    const xlsx_path_latest =
-        'https://www.bsg.ox.ac.uk/sites/default/files/OxCGRT_Download_latest_data.xlsx';
-    const writer = fs.createWriteStream(SOURCE_PATH);
+  const xlsx_path_latest =
+    'https://www.bsg.ox.ac.uk/sites/default/files/OxCGRT_Download_latest_data.xlsx';
+  const writer = fs.createWriteStream(SOURCE_PATH);
 
-    const response = await axios({
-        url: xlsx_path_latest,
-        method: 'GET',
-        responseType: 'stream',
-    });
+  const response = await axios({
+    url: xlsx_path_latest,
+    method: 'GET',
+    responseType: 'stream',
+  });
 
-    response.data.pipe(writer);
+  response.data.pipe(writer);
 
-    return new Promise((resolve, reject) => {
-        writer.on('finish', resolve);
-        writer.on('error', reject);
-    });
+  return new Promise((resolve, reject) => {
+    writer.on('finish', resolve);
+    writer.on('error', reject);
+  });
 }
 
 const parseXLSX = async () => {
-    const parsedExcel = xlsx.parse(SOURCE_PATH)[0].data;
-    console.log(parsedExcel);
+  const parsedExcel = xlsx.parse(SOURCE_PATH)[0].data;
+  console.log(parsedExcel.map((e) => e[0]));
+
+  // simplify data. keep country name, 3-char ISO code,
+  // report date, and stringency index value
+  const simplified = parsedExcel.map((e) => {
+    return {
+      countryName: e[0],
+      countryISO: e[1],
+      date: e[2],
+      index: e[e.length - 2],
+    };
+  });
+  console.log(simplified);
 };
 
 const run = async () => {
-    await downloadLatestXLSX();
-    await parseXLSX();
+  // await downloadLatestXLSX();
+  await parseXLSX();
 };
 
 run();
